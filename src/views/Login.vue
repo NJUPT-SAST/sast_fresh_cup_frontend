54ec5f75d5f1196b6bfc6b8c008c778be874a1c6
Hibiscus_L <1103452257@qq.com>
2019-07-31 12:04:45 +0800

    feat: login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,67 @@
 <template>
-  <div>
-    Hello Login
-  </div>
+  <v-content class="login-container">
+    <v-snackbar
+      v-model="loginError"
+      color="error"
+      :top="true"
+      :timeout="3000"
+    >登录失败，请检查学号或密码是否正确</v-snackbar>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>登录</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="学号"
+                  :clearable="true"
+                  v-model="studentID"
+                  name="login"
+                  prepend-icon="person"
+                  type="text"
+                ></v-text-field>
+                <v-text-field
+                  id="password"
+                  v-model="password"
+                  label="密码"
+                  name="password"
+                  prepend-icon="lock"
+                  type="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="handleLogin">登录</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    studentID: '',
+    password: '',
+    loginError: false,
+  }),
+  methods: {
+    handleLogin() {
+      /** 登录成功 => */
+      /** 登录失败 => */ this.loginError = true;
+    },
+  },
+};
+</script>
+
+<style lang="stylus">
+.login-container
+  height 88vh
+</style>
