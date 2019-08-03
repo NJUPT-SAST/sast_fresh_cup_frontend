484f662f8673c22bf89455670ebf06f6718f7078
XiChi <474504964@qq.com>
2019-08-03 15:24:44 +0800

    homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    Hello HomePage
-  </div>
-</template>
+  <v-card
+    max-width="500"
+    class="mx-auto"
+  >
+    <v-card-title class="card_title">致2019年新生：</v-card-title>
+    <v-card-text>欢迎参加南京邮电大学校科协新生杯
+                <br>点击确定报名参加本次比赛</v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+        <router-link to="Login">
+               <v-btn color="primary">确认</v-btn>
+        </router-link>   
+    </v-card-actions>
+  </v-card>
+</template>、
+
+<script>
+export default {
+  data(){
+     return{}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.mx-auto{
+  margin:150px auto;
+  box-shadow:0 0 8px rgb(131, 132, 133);
+  .card_title{
+    background-color: #1976d2;
+    color:#fff;
+    font-size:20px;
+  }
+  .v-card__text{
+    padding:20px;
+    line-height:35px;
+    font-size:18px;
+  }
+}
+
+</style>
