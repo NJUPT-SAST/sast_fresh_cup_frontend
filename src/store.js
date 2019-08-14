e6ae26d290cecbd4aa654b107d7e288e1a4190a0
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-08-14 18:35:47 +0800

    feat: add api

--- src/store.js
+++ src/store.js
@@ -18,97 +18,102 @@ const injectAnswer = (questionArray, submittedArray) => {
   });
 };
 
+const state = {
+  noticeArray: [],
+  questionArray: [],
+  due: {
+    start: 0,
+    end: 0,
+  },
+  readNoticeArray: [],
+  hash: {
+    problemsMd5: '',
+    noticesMd5: '',
+  },
+  name: '',
+  userinfo: {
+    id: 0,
+    username: '',
+    email: '',
+    isAdmin: 0,
+  },
+  submittedArray: [],
+};
+
+const getters = {
+  unreadNoticeCount(State) {
+    const { noticeArray, readNoticeArray } = State;
+    return noticeArray.length - readNoticeArray.length;
+  },
+};
+
+const mutations = {
+  handleReadNotice(State) {
+    State.readNoticeArray = State.noticeArray;
+  },
+  handleUpdate(State, newState) {
+    Object.keys(newState).forEach((key) => {
+      State[key] = newState[key];
+    });
+  },
+  handleAnswerChange(State, { value, index }) {
+    State.questionArray[index].answer.content = value;
+  },
+  injectCommitted(State, newQuestionArray) {
+    State.questionArray = newQuestionArray;
+  },
+  handleUserinfo(State, data) {
+    Object.keys(data).forEach((key) => {
+      State.userinfo[key] = data[key];
+    });
+  },
+  handleSubmittedInit(State, newSubmitted) {
+    State.submittedArray = newSubmitted;
+  },
+};
+
+const actions = {
+  async update({ commit, state: State }) {
+    const requestList = [];
+    const newState = {};
+    const {
+      data: {
+        name, start, end, problems_md5: problemsMd5, notices_md5: noticesMd5,
+      },
+    } = await getHash();
+    const {
+      name: oldName, due: { start: oldStart, end: oldEnd },
+      hash: { problemsMd5: oldProblemMd5, noticesMd5: oldNoticeMd5 },
+    } = State;
+
+    if (problemsMd5 !== oldProblemMd5) requestList.push(getQuestions);
+    if (noticesMd5 !== oldNoticeMd5) requestList.push(getNotice);
+    if (name !== oldName) newState.name = name;
+    if (start !== oldStart || end !== oldEnd) newState.due = { start, end };
+    newState.hash = { problemsMd5, noticesMd5 };
+
+    const response = await Promise.all(requestList.map(item => item()));
+    if (response.length) {
+      requestList.forEach((item, index) => {
+        if (item === getQuestions) {
+          newState.questionArray = injectAnswer(response[index].data, state.submittedArray);
+        }
+        if (item === getNotice) newState.noticeArray = response[index].data;
+      });
+    }
+
+    commit('handleUpdate', newState);
+  },
+  async init({ dispatch, commit }) {
+    await dispatch('update');
+    const tempQuestionArray = this.state.questionArray;
+    const { data: submitted } = await getSubmitted();
+    const newQuestionArray = injectAnswer(tempQuestionArray, submitted);
+    commit('injectCommitted', newQuestionArray);
+    commit('handleSubmittedInit', submitted);
+  },
+};
+
 export default new Vuex.Store({
-  state: {
-    noticeArray: [],
-    questionArray: [],
-    due: {
-      start: 0,
-      end: 0,
-    },
-    readNoticeArray: [],
-    hash: {
-      problemsMd5: '',
-      noticesMd5: '',
-    },
-    name: '',
-    userinfo: {
-      id: 0,
-      username: '',
-      email: '',
-      isAdmin: 0,
-    },
-    submittedArray: [],
-  },
-  getters: {
-    unreadNoticeCount(state) {
-      const { noticeArray, readNoticeArray } = state;
-      return noticeArray.length - readNoticeArray.length;
-    },
-  },
-  mutations: {
-    handleReadNotice(state) {
-      state.readNoticeArray = state.noticeArray;
-    },
-    handleUpdate(state, newState) {
-      Object.keys(newState).forEach((key) => {
-        state[key] = newState[key];
-      });
-    },
-    handleAnswerChange(state, { value, index }) {
-      state.questionArray[index].answer.content = value;
-    },
-    injectCommitted(state, newQuestionArray) {
-      state.questionArray = newQuestionArray;
-    },
-    handleUserinfo(state, data) {
-      Object.keys(data).forEach((key) => {
-        state.userinfo[key] = data[key];
-      });
-    },
-    handleSubmittedInit(state, newSubmitted) {
-      state.submittedArray = newSubmitted;
-    },
-  },
-  actions: {
-    async update({ commit, state }) {
-      const requestList = [];
-      const newState = {};
-      const {
-        data: {
-          name, start, end, problems_md5: problemsMd5, notices_md5: noticesMd5,
-        },
-      } = await getHash();
-      const {
-        name: oldName, due: { start: oldStart, end: oldEnd },
-        hash: { problemsMd5: oldProblemMd5, noticesMd5: oldNoticeMd5 },
-      } = state;
-
-      if (problemsMd5 !== oldProblemMd5) requestList.push(getQuestions);
-      if (noticesMd5 !== oldNoticeMd5) requestList.push(getNotice);
-      if (name !== oldName) newState.name = name;
-      if (start !== oldStart || end !== oldEnd) newState.due = { start, end };
-      newState.hash = { problemsMd5, noticesMd5 };
-
-      const response = await Promise.all(requestList.map(item => item()));
-      if (response.length) {
-        requestList.forEach((item, index) => {
-          if (item === getQuestions) {
-            newState.questionArray = injectAnswer(response[index].data, state.submittedArray);
-          }
-          if (item === getNotice) newState.noticeArray = response[index].data;
-        });
-      }
-
-      commit('handleUpdate', newState);
-    },
-    async init({ dispatch, commit }) {
-      await dispatch('update');
-      const tempQuestionArray = this.state.questionArray;
-      const { data: submitted } = await getSubmitted();
-      const newQuestionArray = injectAnswer(tempQuestionArray, submitted);
-      commit('injectCommitted', newQuestionArray);
-      commit('handleSubmittedInit', submitted);
-    },
-  },
+  state, getters, mutations, actions,
 });
