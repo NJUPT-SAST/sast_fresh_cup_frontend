4452ef4dccea3fd22806b469f63b6bd92965d86e
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-11-18 10:34:43 +0800

    fix: memory leak

--- src/utils/globalNotification.js
+++ src/utils/globalNotification.js
@@ -1,6 +1,8 @@
 export default function globanNotification(title, body) {
   const icon = '/favicon.ico';
   if ('Notification' in window) {
+    // 用户拒绝通知弹窗后，是不能再requestPermission的，导致无限递归。8说了，问就是菜
+    if (Notification.permission === 'denied') return;
     if (Notification.permission !== 'granted') {
       Notification.requestPermission(() => {
         globanNotification(title, body);
