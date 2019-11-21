7d688b664619c002cc251f843a7e9e4a1c2b8e32
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-11-21 20:35:55 +0800

    feat: fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const baseURL = process.env.VUE_APP_API_REQUEST_URL || `http://${location.hostname}:8000`; // 开发环境地址
+const { port, hostname } = window.location;
+
+export const baseURL = process.env.VUE_APP_API_REQUEST_URL || `http://${hostname}${port ? `:${port}` : ''}`; // 开发环境地址
 const clientSecret = process.env.VUE_APP_API_SECRET || 'lDCcVZDe8aPgBKK2Z3FfUXUMDHLXBj49GVB4ArkA'; // 密钥
 
 let instance = axios.create({
